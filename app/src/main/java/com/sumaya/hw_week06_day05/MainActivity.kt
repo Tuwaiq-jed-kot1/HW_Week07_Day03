a7dc7ac1bee806ba6d3c5b6a713fc01326604ab2
Sumaya Altuwairqi <sumaya.altuwairqi@gmail.com>
2021-11-04 19:03:05 +0300

    init push

---
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sumaya.hw_week06_day05
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
