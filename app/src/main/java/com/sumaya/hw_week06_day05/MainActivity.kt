defd7d82c30b994279aee74031de131d1e64c73a
Sumaya Altuwairqi <91243918+Altuwairqi-SA@users.noreply.github.com>
2021-11-09 16:38:35 +0300

    done

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sumaya.hw_week06_day05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        }
     }
-}
