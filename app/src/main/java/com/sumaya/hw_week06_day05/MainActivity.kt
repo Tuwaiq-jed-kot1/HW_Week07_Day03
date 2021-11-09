76a5d49e7430925fe14b80f9e0e0a7d4cf00dca4
Sumaya Altuwairqi <91243918+altuwairqi-sa@users.noreply.github.com>
2021-11-09 16:38:35 +0300

    Done

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sumaya.hw_week06_day05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var cView :Signature
+    lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cView = findViewById(R.id.signature)
+        btn = findViewById(R.id.button)
     }
-}
+    }
