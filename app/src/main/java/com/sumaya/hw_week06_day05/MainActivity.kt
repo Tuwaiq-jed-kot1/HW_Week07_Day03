b78140198f06000963eef597039df4c02a1e136c
FQaht <Faisal.Alqahtani.jed.k1@tuwaiq.edu.sa>
2021-11-10 08:12:01 +0300

    DONE!

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -2,10 +2,15 @@ package com.sumaya.hw_week06_day05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sign: SignatureView
+    private lateinit var btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sign = findViewById(R.id.signatureView)
+        btn = findViewById(R.id.button)
     }
 }
---
+++ app/src/main/java/com/sumaya/hw_week06_day05/SignatureView.kt
@@ -0,0 +1,61 @@
+package com.sumaya.hw_week06_day05
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.drawable.ColorDrawable
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class SignatureView (context: Context, attrs: AttributeSet?=null): View(context,attrs){
+    private var line: Path=Path()
+    val lines = mutableListOf<Path>()
+
+    var sPaintPaint = Paint().apply {
+        color = 0xff000000.toInt()
+        style =Paint.Style.STROKE
+        strokeWidth=7F
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y=event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+
+                line.moveTo(x,y)
+            }
+            MotionEvent.ACTION_MOVE -> {
+
+                line.lineTo(x,y)
+            }
+            MotionEvent.ACTION_UP -> {
+                lines.add(line)
+
+            }
+            MotionEvent.ACTION_CANCEL-> {
+
+            }
+
+        }
+        invalidate()
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if ((this.background as ColorDrawable).equals(resources.getColor(R.color.black))) {
+            sPaintPaint.color=0xffFFFFFF.toInt()
+            for (i in lines) {
+                canvas.drawPath(i, sPaintPaint)
+            }
+        } else {
+            for (i in lines) {
+                canvas.drawPath(i, sPaintPaint)
+            }
+        }
+    }
+
+}
