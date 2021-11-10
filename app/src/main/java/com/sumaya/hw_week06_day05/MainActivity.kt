de0c028dc0dc6554dd6b5a9f3ceec7e814ab5e41
MahilaAlmutairi <Mahila.Almutairi.jed.k1@tuwaiq.edu.sa>
2021-11-10 05:17:00 +0300

    HW3_W7

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sumaya.hw_week06_day05
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/sumaya/hw_week06_day05/SignatureView.kt
@@ -0,0 +1,40 @@
+package com.sumaya.hw_week06_day05
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class SignatureView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+    private var path = Path()
+    var signaturePaint = Paint().apply {
+        isAntiAlias = true
+        color = ResourcesCompat.getColor(resources, R.color.black, null)
+        style = Paint.Style.STROKE
+        strokeWidth = 12f
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val dX = event.x
+        val dY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> path.moveTo(dX, dY)
+            MotionEvent.ACTION_MOVE -> path.lineTo(dX, dY)
+            else -> return false
+        }
+        postInvalidate()
+        return true
+
+    }
+
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPath(path, signaturePaint);
+
+    }
+
+}
