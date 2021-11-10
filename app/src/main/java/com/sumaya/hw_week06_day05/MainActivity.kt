b6205a547b4d99e19066bfb4980200342bbc75f3
rubaomar <Rr094794003>
2021-11-10 08:23:47 +0300

    week7.day3

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sumaya.hw_week06_day05
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var changeBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        changeBtn = findViewById(R.id.layout.Btn)
+
     }
 }
