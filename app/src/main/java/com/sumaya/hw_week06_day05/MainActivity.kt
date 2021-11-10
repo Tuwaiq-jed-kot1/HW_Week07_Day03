b3ccd62ae8571b15785b34f03633a05109776619
Bashayer Alosaimi <bashayer.alosaimi37@gmail.com>
2021-11-10 09:01:17 +0300

    Done

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.sumaya.hw_week06_day05
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Xml
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var customView :SignView
+    lateinit var botton:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        customView =findViewById(R.id.signatureView)
+        botton =findViewById(R.id.button)
+
     }
 }
