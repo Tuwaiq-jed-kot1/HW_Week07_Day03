e02c75e7316a67fd9c6eed7d8df58bea502c17fc
Marah <marah.bossi.jed.k1@tuwaiq.edu.sa>
2021-11-10 09:51:04 +0300

    done

--- app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
+++ app/src/main/java/com/sumaya/hw_week06_day05/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.sumaya.hw_week06_day05
 
+import android.graphics.Color
+import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
