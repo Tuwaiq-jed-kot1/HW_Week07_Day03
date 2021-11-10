0c34c1f0c94f6789b4c73b80f016422738358867
roulaallubdi <Roula.Allubdi.jed.k1@tuwaiq.edu.sa>
2021-11-10 09:29:00 +0300

    Done!

---
+++ app/src/main/java/com/sumaya/hw_week06_day05/SignatureCustomView.kt
@@ -0,0 +1,35 @@
+package com.sumaya.hw_week06_day05
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class SignatureCustomView (context: Context, attrs: AttributeSet? = null) : View(context,attrs){
+
+    private val path = Path()
+    private val paint = Paint()
+    init {
+        paint.strokeWidth = 15f
+        paint.isAntiAlias = true
+        paint.style=Paint.Style.STROKE
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action){
+            MotionEvent.ACTION_DOWN -> path.moveTo(event.x,event.y)
+            MotionEvent.ACTION_MOVE -> path.lineTo(event.x,event.y)
+            MotionEvent.ACTION_UP -> {}
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.drawPath(path,paint)
+    }
+
+}
