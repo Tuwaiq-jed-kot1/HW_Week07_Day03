4f5fdcf79e7ddb6db949efa5e0daea1d51295bc8
musab <mas.gh123@hotmail.com>
2021-11-12 17:55:18 +0300

    ...

---
+++ app/src/main/java/com/sumaya/hw_week06_day05/DrawCustomView.kt
@@ -0,0 +1,47 @@
+package com.sumaya.hw_week06_day05
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawCustomView(context: Context, attrs: AttributeSet? = null) : View(context,attrs){
+    var paint: Paint? = null
+    var path: Path? = null
+
+    init {
+        paint = Paint()
+        path = Path()
+        paint!!.color = Color.BLACK
+        paint!!.strokeWidth = 10f
+        paint!!.style = Paint.Style.STROKE
+        paint!!.isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas!!.drawPath(path!!,paint!!)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val xpos:Float=event!!.x
+        val ypos:Float=event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> {
+                path!!.moveTo(xpos, ypos)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                path!!.lineTo(xpos, ypos)
+            }
+            else -> {
+
+            }
+        }
+        invalidate()
+        return true
+    }
+}
